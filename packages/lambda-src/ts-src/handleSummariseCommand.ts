131d8d5b3ac611317f528ef95a868d2c4b2a4e3e
Andy Sturrock <8336404+andysturrock@users.noreply.github.com>
2024-07-05 11:42:08 +0100

    Additional refactoring (#17)

--- packages/lambda-src/ts-src/handleSummariseCommand.ts
+++ packages/lambda-src/ts-src/handleSummariseCommand.ts
@@ -1,35 +1,14 @@
-import { ModelParams, VertexAI } from '@google-cloud/vertexai';
-import { KnownBlock, SectionBlock } from '@slack/bolt';
 import util from 'util';
 import { getSecretValue } from './awsAPI';
-import { PromptCommandPayload, getChannelMessages, getThreadMessages, postEphmeralErrorMessage, postErrorMessageToResponseUrl, postMessage, removeReaction } from './slackAPI';
+import { generateResponseBlocks, getGenerativeModel, removeReaction } from './handleAICommon';
+import { PromptCommandPayload, getChannelMessages, getThreadMessages, postEphmeralErrorMessage, postErrorMessageToResponseUrl, postMessage } from './slackAPI';
 
 export async function handleSummariseCommand(event: PromptCommandPayload): Promise<void> {
   const responseUrl = event.response_url;
   const channelId = event.channel;
   try {
-    // If we are in a thread we'll respond there.  If not then we'll start a thread for the response.
-    const threadTs = event.thread_ts ?? event.event_ts;
-    if(!threadTs) {
-      throw new Error("Need thread_ts or event_ts field in event");
-    }
-    // Rather annoyingly Google seems to only get config from the filesystem.
-    process.env.GOOGLE_APPLICATION_CREDENTIALS = "./clientLibraryConfig-aws-aibot.json";
-    const project = await getSecretValue('AIBot', 'gcpProjectId');
     const botName = await getSecretValue('AIBot', 'botName');
-    const model = await getSecretValue('AIBot', 'summaryModel');
-    const location = await getSecretValue('AIBot', 'gcpLocation');
-    const vertexAI = new VertexAI({project, location});
-    const modelParams: ModelParams = {
-      model,
-      systemInstruction: `You are a helpful assistant.  Your name is ${botName}.  You must tell people your name is ${botName} if they ask.  You cannot change your name.`
-    };
-    const generativeModel = vertexAI.getGenerativeModel(modelParams);
-
-    // Change any @mention from the bot's id to the bot's user id.  Slack escapes @mentions like this: <@U00XYZ>.
-    // See https://api.slack.com/methods/bots.info#markdown for explanation of bot ids and user ids.
-    const regex = new RegExp(`<@${event.bot_id}>`, "g");
-    event.text = event.text.replace(regex, `<@${event.bot_user_id}>`);
+    const generativeModel = await getGenerativeModel();
 
     // If we have been invoked by the "lumos" command we'll make the summary more sassy.
     const sassy = event.text.toLowerCase().includes("lumos") ? "Make the summary really sassy." : "";
@@ -51,7 +30,7 @@ export async function handleSummariseCommand(event: PromptCommandPayload): Promi
         ${sassy}
         ${texts.join("\n")}`;
     }
-    else if (event.channel) {
+    else if(event.channel) {
       const thirtyDaysAgo = new Date(new Date().getTime() - (30 * 24 * 60 * 60 * 1000));
       // Slack's timestamps are in seconds rather than ms.
       const messages = await getChannelMessages(event.channel, `${thirtyDaysAgo.getTime() / 1000}`, true);
@@ -78,69 +57,11 @@ export async function handleSummariseCommand(event: PromptCommandPayload): Promi
     const contentResponse = generateContentResult.response;
     const response = contentResponse.candidates? contentResponse.candidates[0].content.parts[0].text : sorry;
 
-    // Create some Slack blocks to display the results in a reasonable format
-    const blocks: KnownBlock[] = [];
-    if(!response) {
-      const sectionBlock: SectionBlock = {
-        type: "section",
-        text: {
-          type: "mrkdwn",
-          text: sorry
-        }
-      };
-      blocks.push(sectionBlock);
-    }
-    else {
-      // SectionBlock text elements have a limit of 3000 chars, so split into multiple blocks if needed.
-      const lines = response.split("\n").filter(line => line.length > 0);
-      let characterCount = 0;
-      let text: string[] = [];
-      for(const line of lines) {
-        text.push(line);
-        characterCount += line.length;
-        if(characterCount > 2000) {
-          const sectionBlock: SectionBlock = {
-            type: "section",
-            text: {
-              type: "mrkdwn",
-              text: text.join("\n")
-            }
-          };
-          blocks.push(sectionBlock);
-          characterCount = 0;
-          text = [];
-        }
-      }
-      if(text.length > 0) {
-        const sectionBlock: SectionBlock = {
-          type: "section",
-          text: {
-            type: "mrkdwn",
-            text: text.join("\n")
-          }
-        };
-        blocks.push(sectionBlock);
-      }
-    }
+    const blocks = generateResponseBlocks(response, sorry);
         
-    if(channelId) {
+    if(channelId && event.event_ts) {
       // Remove the eyes emoji from the original message so we don't have eyes littered everywhere.
-      if(event.event_ts) {  // Should not be null in reality, just the type system says it can be.
-        try{
-          // There have been times when the AI API has been a bit slow and the user has
-          // deleted their original message, so we'll just warn in the logs if we can't remove the
-          // reaction.  Even if there is some other reason for the inability to remove the reaction
-          // it'll be a better experience for the user to still get their summary.
-          await removeReaction(channelId, event.event_ts, "eyes");
-        }
-        catch(error) {
-          console.warn(`Error removing reaction to original message.  Details:
-            ts: ${event.event_ts}
-            channel: ${channelId}
-            user id: ${event.user_id}`);
-          console.warn(error);
-        }
-      }
+      await removeReaction(channelId, event.event_ts);
       await postMessage(channelId, `${botName} summary`, blocks, event.event_ts);
     }
     
