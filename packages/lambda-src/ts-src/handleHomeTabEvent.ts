ea822b4432087279d0979c065be399e9c12b5a8b
Andy Sturrock <8336404+andysturrock@users.noreply.github.com>
2024-08-11 10:54:19 +0100

    Update app's Home tab with auth status and a button if not authed.

--- packages/lambda-src/ts-src/handleHomeTabEvent.ts
+++ packages/lambda-src/ts-src/handleHomeTabEvent.ts
@@ -1,17 +1,56 @@
-import { AppHomeOpenedEvent, KnownBlock, MrkdwnElement, SectionBlock } from '@slack/bolt';
+import { ActionsBlock, AppHomeOpenedEvent, KnownBlock, MrkdwnElement, SectionBlock } from '@slack/bolt';
+import { getSecretValue } from './awsAPI';
 import { publishHomeView } from './slackAPI';
+import { getAccessToken } from './tokensTable';
 
 export async function handleHomeTabEvent(event: AppHomeOpenedEvent) {
   const blocks: KnownBlock[] = [];
 
-  const mrkdwnElement: MrkdwnElement = {
-    type: 'mrkdwn',
-    text: 'Placeholder'
-  };
-  const sectionBlock: SectionBlock = {
-    type: 'section',
-    text: mrkdwnElement
-  };
-  blocks.push(sectionBlock);
+  const botName = await getSecretValue('AIBot', 'botName');
+
+  const accessToken = await getAccessToken(event.user);
+
+  if(accessToken) {
+    const mrkdwnElement: MrkdwnElement = {
+      type: 'mrkdwn',
+      text: `You are authorised with ${botName}`
+    };
+    const sectionBlock: SectionBlock = {
+      type: 'section',
+      text: mrkdwnElement
+    };
+    blocks.push(sectionBlock);
+  }
+  else {
+    const authUrl = await getSecretValue('AIBot', 'authUrl');
+
+    const mrkdwnElement: MrkdwnElement = {
+      type: 'mrkdwn',
+      text: `You are not authorised with ${botName}.  Use the button below to authorise.`
+    };
+    const sectionBlock: SectionBlock = {
+      type: 'section',
+      text: mrkdwnElement
+    };
+    blocks.push(sectionBlock);
+    const actionsBlock: ActionsBlock = {
+      type: "actions",
+      block_id: "authButton",
+      elements: [
+        {
+          type: "button",
+          text: {
+            type: "plain_text",
+            text: `Authorise ${botName}`
+          },
+          url: authUrl,
+          style: "primary",
+          action_id: 'authButton'
+        }
+      ]
+    };
+    blocks.push(actionsBlock);
+  }
+
   await publishHomeView(event.user, blocks);
 }
