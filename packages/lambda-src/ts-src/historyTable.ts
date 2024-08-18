6fe4eb88ad78b26695bbf31df134d939c293ad5f
Andy Sturrock <8336404+andysturrock@users.noreply.github.com>
2024-08-18 13:51:29 +0100

    Store history/content for each agent separately rather than handing the entire conversation to the sub agents.  This is much less confusing for the sub agents.  The supervisor also gets its own history where it can see the user prompts and the answers from the sub agents. (#26)

--- packages/lambda-src/ts-src/historyTable.ts
+++ packages/lambda-src/ts-src/historyTable.ts
@@ -8,20 +8,24 @@ const TTL_IN_MS = 1000 * 60 * 60 * 24 * 30; // Thirty days
 const TableName = "AIBot_History";
 
 export type History = {
-  slack_id: string,
+  channel_id: string,
   thread_ts: string,
   content: Content[]
 };
+export type GetHistoryFunction = (channelId: string, threadTs: string, agentName: string) => Promise<Content[] | undefined>;
+export type PutHistoryFunction = (channelId: string, threadTs: string, history: Content[], agentName: string) => Promise<void>;
+
 /**
- * Gets the History for the given user and thread id
- * @param slackId Slack user id 
+ * Gets the History for the given channel and thread id
+ * @param channelId Slack channel id 
  * @param threadTs the thread id for the conversation
- * @returns history or undefined if no history exists for the user and thread
+ * @param agentName the name of the agent to get the history for
+ * @returns history or undefined if no history exists for the channel and thread
  */
-export async function getHistory(slackId: string, threadTs: string) : Promise<Content[] | undefined>  { 
+export async function getHistory(channelId: string, threadTs: string, agentName: string) : Promise<Content[] | undefined>  { 
   const ddbClient = new DynamoDBClient({});
 
-  const id = `${slackId}_${threadTs}`;
+  const id = `${channelId}_${threadTs}_${agentName}`;
 
   const params: QueryCommandInput = {
     TableName,
@@ -41,10 +45,10 @@ export async function getHistory(slackId: string, threadTs: string) : Promise<Co
   }
 }
 
-export async function deleteHistory(slackId: string, threadTs: string) {
+export async function deleteHistory(channelId: string, threadTs: string, agentName: string) {
   const ddbClient = new DynamoDBClient({});
 
-  const id = `${slackId}_${threadTs}`;
+  const id = `${channelId}_${threadTs}_${agentName}`;
 
   const params: DeleteItemCommandInput = {
     TableName,
@@ -59,15 +63,17 @@ export async function deleteHistory(slackId: string, threadTs: string) {
 }
 
 /**
- * Put (ie save new or overwite) history with slackId and threadTs as the key
- * @param slackId Key for the table
+ * Put (ie save new or overwite) history with channelId and threadTs as the key
+ * @param channelId channel id
+ * @param threadTs thread timestamp
+ * @param agentName the name of the agent whose history this is
  * @param history history to write
  */
-export async function putHistory(slackId: string, threadTs: string, history: Content[]) {
+export async function putHistory(channelId: string, threadTs: string, history: Content[], agentName: string) {
   const now = Date.now();
   const ttl = new Date(now + TTL_IN_MS);
 
-  const id = `${slackId}_${threadTs}`;
+  const id = `${channelId}_${threadTs}_${agentName}`;
 
   const putItemCommandInput: PutItemCommandInput = {
     TableName,
