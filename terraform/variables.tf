c59e41b75f5dc81f68a30b066147601e7ef38d1b
Andy Sturrock <8336404+andysturrock@users.noreply.github.com>
2026-01-16 18:10:35 +0000

    chore: flatten terraform structure and re-order deployment to handle registry bootstrap

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,3 @@
-
-
 variable "gcp_gemini_project_id" {
   type        = string
   description = "GCP id for Gemini project"
@@ -10,23 +8,19 @@ variable "gcp_gemini_project_number" {
   description = "GCP project number for Gemini project"
 }
 
-
-
-
-
 variable "gcp_region" {
   type        = string
-  description = "GCP region"
+  description = "GCP region, eg europe-west2"
 }
 
 variable "gcp_zone" {
   type        = string
   description = "GCP zone"
+  default     = "europe-west2-a"
 }
-
-
 
 variable "enable_gemini_apis" {
   type        = bool
   description = "Enable Gemini APIs"
+  default     = true
 }
